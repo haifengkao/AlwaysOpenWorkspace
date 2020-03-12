8a1f6adea4bbd981edcaf2a670bacd546f4b017f
Hai Feng <haifeng@cocoaspice.in>
2020-03-13 03:15:51 +0800

    first version done

--- README.md
+++ README.md
@@ -1,65 +1,18 @@
-# autoaddfolder README
+# AlwaysOpenWorkspace
 
-This is the README for your extension "autoaddfolder". After writing up a brief description, we recommend including the following sections.
+Inspired by [vim-rooter](https://github.com/airblade/vim-rooter). AlwaysOpenWorkspace opens a workspace by finding the project root when you open a file.
 
-## Features
+The project root is identified by:
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+* being a known directory;
+* or the presence of a known directory, such as a VCS directory;
+* or the presence of a known file, such as a Rakefile.
 
-For example if there is an image subfolder under your extension project workspace:
+Out of the box it knows about git, darcs, mercurial, bazaar, and subversion, but you can configure it to look for anything.
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+The workspace will not be opened if the file has been associated with any existing workspace. 
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+### 0.0.1
+- Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,87 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as findUp from 'find-up';
+import * as path from 'path';
+
+var loggingEnabled = false;
+
+export function dumpInConsole(...args: any[]) {
+    if (loggingEnabled) {
+        console.log(...args);
+    }
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autoaddfolder" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "AlwaysOpenWorkspace" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    const disposable = vscode.workspace.onDidOpenTextDocument( async (textDocument) => {
+        const isEnabled: boolean | undefined = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.enabled");
+        const logEnabled: boolean | undefined  = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.loggingEnabled");
+        const rootFolders: string[] | undefined  = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.rootFolders");
+    
+        // if enabled is not defined or false then don't do anything...
+        if (logEnabled != undefined) {
+            loggingEnabled = logEnabled
+        }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+        if (!isEnabled) {
+            dumpInConsole("AlwaysOpenWorkspace disabled");
+            return;
+        }
 
-	context.subscriptions.push(disposable);
+        if (!rootFolders) { 
+            dumpInConsole("AlwaysOpenWorkspace rootFolders are not set");
+            return;
+        }
+
+        const fileUrl = textDocument.uri;
+        const filePath = fileUrl.fsPath;
+
+        if (fileUrl.scheme != 'file') { return; } // it might be git or output scheme, not a real file
+
+        dumpInConsole("AlwaysOpenWorkspace didOpen: ", fileUrl, filePath);
+        const workspace = vscode.workspace.getWorkspaceFolder(fileUrl)
+        if (workspace) { 
+            dumpInConsole("AlwaysOpenWorkspace has worksapce just return: ", filePath);
+            return; 
+        } // we had the worksapce, nothing to do
+
+        const dirname = path.dirname(filePath)
+        for (var name of rootFolders) { 
+            const typeValue = (name.slice(-1) == "/") ? 'directory' : 'file';
+            const folderPath = await findUp(name, {type: typeValue, cwd: dirname});
+            if (folderPath) {
+                const parent = path.dirname(folderPath);
+                const parentUrl = vscode.Uri.file(parent)
+                dumpInConsole("AlwaysOpenWorkspace find folder: ", folderPath, parentUrl);
+                vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0, null, { uri: parentUrl, name: parent})
+                return; // finished
+            } else { 
+                dumpInConsole("AlwaysOpenWorkspace failed to find: ", name, folderPath);
+            }
+        }
+    });
+
+    const disposable2 = vscode.workspace.onDidCloseTextDocument((textDocument) => {
+        const isEnabled: boolean | undefined = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.enabled");
+        if (!isEnabled) {
+            dumpInConsole("AlwaysOpenWorkspace disabled");
+            return;
+        }
+
+        const fileUrl = textDocument.uri;
+        const filePath = fileUrl.path;
+        const workspace = vscode.workspace.getWorkspaceFolder(fileUrl)
+    });
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
