f2e5a7902d267da3c55b2c2d76ad68b60abc855e
Hai Feng <haifeng@cocoaspice.in>
2020-03-24 11:49:51 +0800

    add autoclose

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import * as findUp from 'find-up';
 import * as path from 'path';
+import {  TextDocument, Uri } from 'vscode';
 
 var loggingEnabled = false;
 
@@ -12,13 +13,48 @@ export function dumpInConsole(...args: any[]) {
     }
 }
 
+export function userDocuments(): Array<Uri>
+{ 
+    return vscode.workspace.textDocuments.map(doc => doc.uri).filter(uri => uri.scheme == 'file');
+}
+
+export function closeDeadFolders()
+{ 
+    const isAutoClose: boolean | undefined = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.autoClose");
+    if (!isAutoClose) { return; }
+
+    const docUris = userDocuments()
+    if (docUris.length <= 0) { return; } // don't close the last workspace
+
+    const liveWorkspaces = new Set(docUris.map(function(fileUrl) { 
+        return vscode.workspace.getWorkspaceFolder(fileUrl);
+    }).filter(x => x != null)
+    );    
+
+    const liveWorkspacesArray = Array.from(liveWorkspaces);
+    const allWorkspaces = vscode.workspace.workspaceFolders || [];
+
+    let diff = new Set(allWorkspaces.filter(x => !liveWorkspaces.has(x)));
+    let toBeRemoved = diff.values().next().value;
+
+    if (toBeRemoved) {
+        dumpInConsole("AlwaysOpenWorkspace close workspace: ", toBeRemoved, liveWorkspacesArray, allWorkspaces, diff.values());
+        vscode.workspace.updateWorkspaceFolders(toBeRemoved.index, 1);
+    }
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "AlwaysOpenWorkspace" is now active!');
+    const logEnabled: boolean | undefined  = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.loggingEnabled");
+    if (logEnabled != undefined) {
+        loggingEnabled = logEnabled
+    }
+
+    closeDeadFolders();
 
     const disposable = vscode.workspace.onDidOpenTextDocument( async (textDocument) => {
         const isEnabled: boolean | undefined = vscode.workspace.getConfiguration().get("AlwaysOpenWorkspace.enabled");
@@ -30,6 +66,7 @@ export function activate(context: vscode.ExtensionContext) {
             loggingEnabled = logEnabled
         }
 
+        //dumpInConsole("AlwaysOpenWorkspace log: ", vscode.workspace.name, vscode.workspace.workspaceFile, vscode.workspace.textDocuments);
         if (!isEnabled) {
             dumpInConsole("AlwaysOpenWorkspace disabled");
             return;
@@ -48,9 +85,9 @@ export function activate(context: vscode.ExtensionContext) {
         dumpInConsole("AlwaysOpenWorkspace didOpen: ", fileUrl, filePath);
         const workspace = vscode.workspace.getWorkspaceFolder(fileUrl)
         if (workspace) { 
-            dumpInConsole("AlwaysOpenWorkspace has worksapce just return: ", filePath);
+            dumpInConsole("AlwaysOpenWorkspace has workspace just return: ", filePath);
             return; 
-        } // we had the worksapce, nothing to do
+        } // we had the workspace, nothing to do
 
         const dirname = path.dirname(filePath)
         for (var name of rootFolders) { 
@@ -63,7 +100,7 @@ export function activate(context: vscode.ExtensionContext) {
                 vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0, null, { uri: parentUrl, name: parent})
                 return; // finished
             } else { 
-                dumpInConsole("AlwaysOpenWorkspace failed to find: ", name, folderPath);
+                //dumpInConsole("AlwaysOpenWorkspace failed to find: ", name, folderPath);
             }
         }
     });
@@ -76,8 +113,11 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         const fileUrl = textDocument.uri;
-        const filePath = fileUrl.path;
-        const workspace = vscode.workspace.getWorkspaceFolder(fileUrl)
+        const filePath = fileUrl.fsPath;
+        if (fileUrl.scheme != 'file') { return; } // it might be git or output scheme, not a real file
+
+        dumpInConsole("AlwaysOpenWorkspace didClose: ", fileUrl, filePath);
+        closeDeadFolders();
     });
 
     context.subscriptions.push(disposable);
